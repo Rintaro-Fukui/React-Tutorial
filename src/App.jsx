a64d48e7278a03d041a1ed9447c9fdd1db9dde6d
Rintaro-Fukui <zrdf813@icloud.com>
2022-12-27 19:16:18 +0900

    色々分割した

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-function App() {
+function Header() {
     return (
-        <div>
-            <header className="hero is-dark is-bold">
-                <div className="hero-body">
-                    <div className="container">
-                        <h1 className="title">ｲｯﾇ</h1>
-                    </div>
+        <header className="hero is-dark is-bold">
+            <div className="hero-body">
+                <div className="container">
+                    <h1 className="title">ｲｯﾇ</h1>
                 </div>
-            </header>
-            <main>
-                <section className="section">
-                    <div className="container">
-                        <div className="columns is-vcentered is-multiline">
-                            <div className="column is-3">
-                                <div className="card">
-                                    <div className="card-image">
-                                        <figure className="image">
-                                            <img 
-                                                src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg" 
-                                                alt="cute dog" 
-                                            />
-                                        </figure>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </main>
-            <footer className="footer">
+            </div>
+        </header>
+    );
+}
+
+function Image() {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image">
+                    <img 
+                        src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg" 
+                        alt="cute dog" 
+                    />
+                </figure>
+            </div>
+        </div>
+    );
+}
+
+function Gallery() {
+    return (
+        <div className="columns is-vcentered is-multiline">
+            <div className="column is-3">
+                <Image />
+            </div>
+        </div>
+    );
+}
+
+function Main() {
+    return (
+        <main>
+            <section className="section">
+                <div className="container">
+                    <Gallery />
+                </div>
+            </section>
+        </main>
+    );
+}
+
+function Footer() {
+    return (
+        <footer className="footer">
                 <div className="content has-text-centered">
                     <p>Dog images are retrieved from Dog API</p>
                     <p>
@@ -36,8 +57,18 @@ function App() {
                     </p>
                 </div>
             </footer>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Footer />
         </div>
     );
 }
+
 
 export default App;
